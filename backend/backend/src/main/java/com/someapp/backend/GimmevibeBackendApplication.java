b918026f37d77c2d1cba1dbcb54c8e5ea8590d4a
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2021-03-24 09:12:34 +0200

    Change to folder structure

---
+++ backend/backend/src/main/java/com/someapp/backend/GimmevibeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.someapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimmevibeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimmevibeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/someapp/backend/GimmevibeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.someapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimmevibeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
