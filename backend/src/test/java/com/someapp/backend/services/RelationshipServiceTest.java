f76d442fb1ca21a26b98342b8c02b9e44f679f4d
arto.mulkahainen <arto.mulkahainen@gmail.com>
2022-04-26 19:53:07 +0300

    RelationshipServiceTest updated

--- backend/src/test/java/com/someapp/backend/services/RelationshipServiceTest.java
+++ backend/src/test/java/com/someapp/backend/services/RelationshipServiceTest.java
@@ -1,13 +1,24 @@
 package com.someapp.backend.services;
 
+import com.someapp.backend.interfaces.repositories.RelationshipRepository;
 import com.someapp.backend.utils.jwt.JWTTokenUtil;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
@@ -18,5 +29,12 @@ public class RelationshipServiceTest {
     @Mock
     private HttpServletRequest req;
     @Mock
-    private RelationshipService relationshipService;
+    private RelationshipRepository relationshipRepository;
+    @InjectMocks
+    private RelationshipServiceImpl relationshipService;
+
+    @Test
+    void saveIsSuccessful() {
+        when(relationshipRepository.findRelationshipsByUniqueId(any())).thenReturn(new ArrayList<>());
+    }
 }
