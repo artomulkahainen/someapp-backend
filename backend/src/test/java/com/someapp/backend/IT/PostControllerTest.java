62553c9eaa7cc6954a005e110d2730a3a3623285
arto.mulkahainen <arto.mulkahainen@gmail.com>
2022-06-01 22:35:39 +0300

    PostIT tests created

--- backend/src/test/java/com/someapp/backend/IT/PostControllerTest.java
+++ backend/src/test/java/com/someapp/backend/IT/PostControllerTest.java
@@ -1,62 +1,154 @@
-/*package com.someapp.backend.controllers;
+package com.someapp.backend.IT;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.Matchers.*;
-
-import com.someapp.backend.entities.Post;
+import com.someapp.backend.dto.DeletePostRequest;
+import com.someapp.backend.dto.SendPostRequest;
 import com.someapp.backend.repositories.PostCommentRepository;
 import com.someapp.backend.repositories.PostLikeRepository;
 import com.someapp.backend.repositories.PostRepository;
-import com.someapp.backend.repositories.UserRepository;
-import com.someapp.backend.util.Format;
-import com.someapp.backend.util.TestData;
-import com.someapp.backend.util.requests.SendPostRequest;
+import com.someapp.backend.utils.requests.LoginRequest;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-@ActiveProfiles("test")
+import static com.someapp.backend.testUtility.Format.asJsonString;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class PostControllerTest {
 
     @Autowired
-    private PostRepository postRepository;
-
+    private WebApplicationContext context;
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PostLikeRepository postLikeRepository;
-
+    private PostRepository repository;
     @Autowired
     private PostCommentRepository postCommentRepository;
-
     @Autowired
-    private MockMvc mockMvc;
-
-    private TestData testData;
+    private PostLikeRepository postLikeRepository;
+    private MockMvc mvc;
+    private String token;
 
     @Before
-    public void initializeTestData() throws Exception {
-        testData = new TestData();
-        testData.createPostTestData(postRepository, userRepository);
+    @Transactional
+    public void setup() throws Exception {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+        // LOGIN FIRST AND STORE THE TOKEN FROM LOGIN
+        MockHttpServletResponse response = mvc
+                .perform(post("/loginByUsingPOST")
+                        .content(asJsonString(new LoginRequest("kalleKustaa", "korkki")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andReturn().getResponse();
+        token = new JSONObject(response.getContentAsString()).getString("token");
+    }
+
+    @Test
+    @WithMockUser(username = "kalleKustaa")
+    @Transactional
+    @Sql(value = {"/db/users.sql", "/db/posts.sql"})
+    public void creatingNewPostIsPossible() throws Exception {
+        MockHttpServletResponse response = mvc
+                .perform(post("/sendNewPostByUsingPOST")
+                        .with(request -> {
+                            request.addHeader("Authorization", "Bearer " + token);
+                            return request;
+                        })
+                        .content(asJsonString(new SendPostRequest("Hulabaloo")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.createdDate").isNotEmpty())
+                .andExpect(jsonPath("$.post").value("Hulabaloo"))
+                .andExpect(jsonPath("$.userUuid").value("609b08a3-356d-40d8-9a87-b4e1d47abf4d"))
+                .andReturn()
+                .getResponse();
+
+        UUID newPost = UUID.fromString(new JSONObject(response.getContentAsString()).getString("uuid"));
+        assertTrue(repository.findById(newPost).isPresent());
+    }
+
+    @Test
+    @WithMockUser(username = "kalleKustaa")
+    @Transactional
+    @Sql(value = {"/db/users.sql", "/db/posts.sql", "/db/postlikes.sql", "/db/postcomments.sql"})
+    public void deletingPostIsPossible() throws Exception {
+        UUID postId = UUID.fromString("12bffa51-9899-497b-b41a-2b71d8c42629");
+
+        mvc
+                .perform(post("/deletePostByUsingPOST")
+                        .with(request -> {
+                            request.addHeader("Authorization", "Bearer " + token);
+                            return request;
+                        })
+                        .content(asJsonString(new DeletePostRequest(postId)))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Successfully deleted post"));
+
+        assertFalse(repository.findById(postId).isPresent());
+        assertTrue(postCommentRepository.findAll()
+                .stream()
+                .filter(p -> p.getPostId().equals(postId))
+                .collect(Collectors.toList())
+                .isEmpty());
+        assertTrue(postLikeRepository.findAll()
+                .stream()
+                .filter(p -> p.getPostId().equals(postId))
+                .collect(Collectors.toList())
+                .isEmpty());
+    }
+
+    @Test
+    @WithMockUser(username = "kalleKustaa")
+    @Transactional
+    @Sql(value = {"/db/users.sql", "/db/posts.sql"})
+    public void cannotDeleteOtherUsersPost() throws Exception {
+        UUID postId = UUID.fromString("434811ee-f31f-4929-beec-194f237cf417");
+
+        mvc
+                .perform(post("/deletePostByUsingPOST")
+                        .with(request -> {
+                            request.addHeader("Authorization", "Bearer " + token);
+                            return request;
+                        })
+                        .content(asJsonString(new DeletePostRequest(postId)))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.errors[0]")
+                        .value("Post can be deleted only by post creator."));
     }
 
 
+/*
     @Test
     public void sendNewPostSuccessfully() throws Exception {
         mockMvc.perform(post("/posts")
@@ -129,6 +221,6 @@ public class PostControllerTest {
                 .collect(Collectors.toList())
                 .isEmpty());
     }
+*/
 
-
-}*/
+}
--- backend/src/test/resources/db/postlikes.sql
+++ backend/src/test/resources/db/postlikes.sql
@@ -2,3 +2,8 @@
 INSERT INTO post_like(id, created_date, post_id, user_id)
 VALUES('d2985a64-a139-4eb0-a5fb-f356e0fafb66', NOW(),
 '434811ee-f31f-4929-beec-194f237cf417', '609b08a3-356d-40d8-9a87-b4e1d47abf4d');
+
+-- sidney likes kalleKustaa post "Nice stuff" with id 12bffa51-9899-497b-b41a-2b71d8c42629 --
+INSERT INTO post_like(id, created_date, post_id, user_id)
+VALUES('724df059-7e0c-43b2-a2a4-3375605d4098', NOW(),
+'12bffa51-9899-497b-b41a-2b71d8c42629', '30d868a1-e7c9-48da-881f-c6348598b0fd');
