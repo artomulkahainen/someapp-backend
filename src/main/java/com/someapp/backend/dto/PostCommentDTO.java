d3fb0fb2936ce82660ec25a6398261c6cc6c6947
Arto Mulkahainen <arto.mulkahainen@gmail.com>
2023-03-02 20:09:27 +0200

    Add username to postdto and postcomment dto

--- src/main/java/com/someapp/backend/dto/PostCommentDTO.java
+++ src/main/java/com/someapp/backend/dto/PostCommentDTO.java
@@ -4,25 +4,33 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.someapp.backend.dto.abstractDTOs.BaseDTO;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.UUID;
 
 public class PostCommentDTO extends BaseDTO {
 
+    @NotNull
     private final String postComment;
+    @NotNull
     private final UUID postId;
+    @NotNull
     private final UUID userId;
+    @NotNull
+    private final String username;
 
     @JsonCreator
     public PostCommentDTO(@JsonProperty("uuid") final UUID uuid,
                           @JsonProperty("createdDate") final Date date,
                           @JsonProperty("postComment") final String postComment,
                           @JsonProperty("postId") final UUID postId,
-                          @JsonProperty("userId") final UUID userId) {
+                          @JsonProperty("userId") final UUID userId,
+                          @JsonProperty("username") final String username) {
         super(uuid, date);
         this.postComment = postComment;
         this.postId = postId;
         this.userId = userId;
+        this.username = username;
     }
 
     public String getPostComment() {
@@ -35,5 +43,9 @@ public class PostCommentDTO extends BaseDTO {
 
     public UUID getUserId() {
         return userId;
+    }
+
+    public String getUsername() {
+        return this.username;
     }
 }
